9c101a229f6f6dcf1d41011468178104e7eb0901
Bhuwan Shrestha <bhuwan.crestha57@gmail.com>
2024-03-20 11:38:43 +0545

    file upload

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { RouterProvider } from "react-router-dom";
+import { Routers } from "./router";
+import { AppContextProvider } from "./ContextApi";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <React.Suspense fallback={"Loading"}>
+      <AppContextProvider>
+        <RouterProvider router={Routers} />
+      </AppContextProvider>
+    </React.Suspense>
   </React.StrictMode>
 );
 
